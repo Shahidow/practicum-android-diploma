c347e5fa19dfd19955866942055a8b3582655e5d
purpleratchet <111129750+purpleratchet@users.noreply.github.com>
2024-06-10 15:45:02 +0300

    Revert "Создала классы запроса и ответа"

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyResponse.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.data.dto
-
-data class VacancyResponse(
-    val items: List<Vacancy>,
-    val page: Int,
-    val pages: Int,
-    val perPage: Int,
-    val total: Int
-)
