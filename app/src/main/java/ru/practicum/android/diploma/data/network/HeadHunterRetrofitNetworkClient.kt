e89c84a22bf4bf78a023b07c0faddd5ac1530f36
Nadezhda-Gurova <yudinanade@yandex.ru>
2024-06-10 14:46:19 +0300

    HeadHunterRetrofitNetworkClient

--- app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterRetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterRetrofitNetworkClient.kt
@@ -8,25 +8,31 @@ import ru.practicum.android.diploma.data.dto.VacancyResponse
 import java.io.IOException
 
 class HeadHunterRetrofitNetworkClient(private val api: HeadHunterApi) : HeadHunterNetworkClient {
+
+    companion object {
+        private const val AUTHORIZATION_HEADER = "Bearer YOUR_ACCESS_TOKEN"
+        private const val ERROR_CODE = 400
+    }
+
     private fun String.toResponseBody(): ResponseBody {
         return this.toResponseBody(null)
     }
 
     override suspend fun getVacancies(filters: Map<String, String>): Response<VacancyResponse> {
         return try {
-            val response = api.getVacancies("Bearer YOUR_ACCESS_TOKEN", filters)
+            val response = api.getVacancies(AUTHORIZATION_HEADER, filters)
             response
         } catch (e: IOException) {
-            Response.error(400, e.message?.toResponseBody() ?: "".toResponseBody())
+            Response.error(ERROR_CODE, e.message?.toResponseBody() ?: "".toResponseBody())
         }
     }
 
     override suspend fun getVacancy(id: String): Response<VacancyDetails> {
         return try {
-            val response = api.getVacancyDetails("Bearer YOUR_ACCESS_TOKEN", id)
+            val response = api.getVacancyDetails(AUTHORIZATION_HEADER, id)
             response
         } catch (e: IOException) {
-            Response.error(400, e.message?.toResponseBody() ?: "".toResponseBody())
+            Response.error(ERROR_CODE, e.message?.toResponseBody() ?: "".toResponseBody())
         }
     }
 }
