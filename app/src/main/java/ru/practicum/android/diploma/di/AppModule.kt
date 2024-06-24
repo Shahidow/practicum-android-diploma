9f78c9bf039dd163c2cc4288aa3ccbe237a81f6d
dencharski <dencharski@yandex.ru>
2024-06-24 17:49:38 +0200

    -create FilterCountryFragment and FilterCountryViewModel

--- app/src/main/java/ru/practicum/android/diploma/di/AppModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/AppModule.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.favorites.FavoritesViewModel
+import ru.practicum.android.diploma.presentation.filtration.place.country.FilterCountryViewModel
 import ru.practicum.android.diploma.presentation.filtration.FiltrationViewModel
 import ru.practicum.android.diploma.presentation.filtration.industry.IndustryViewModel
 import ru.practicum.android.diploma.presentation.filtration.place.region.FilterRegionViewModel
@@ -20,6 +21,10 @@ val appModule = module {
 
     viewModel<VacancyViewModel> {
         VacancyViewModel(get(), get())
+    }
+
+    viewModel<FilterCountryViewModel> {
+        FilterCountryViewModel()
     }
 
     viewModel<FiltrationViewModel> {
