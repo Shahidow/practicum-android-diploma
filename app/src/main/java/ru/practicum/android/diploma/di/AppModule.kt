293a7aab14c9957927c48d37e46858b22b72a517
dencharski <dencharski@yandex.ru>
2024-06-25 13:46:13 +0200

    - add FilterRegionFragment, FilterRegionViewModel, some edit2

--- app/src/main/java/ru/practicum/android/diploma/di/AppModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/AppModule.kt
@@ -4,9 +4,9 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.favorites.FavoritesViewModel
 import ru.practicum.android.diploma.presentation.filtration.place.country.FilterCountryViewModel
+import ru.practicum.android.diploma.presentation.filtration.place.region.FilterRegionViewModel
 import ru.practicum.android.diploma.presentation.filtration.FiltrationViewModel
 import ru.practicum.android.diploma.presentation.filtration.industry.IndustryViewModel
-import ru.practicum.android.diploma.presentation.filtration.place.region.FilterRegionViewModel
 import ru.practicum.android.diploma.presentation.search.SearchViewModel
 import ru.practicum.android.diploma.presentation.vacancy.VacancyViewModel
 
@@ -21,6 +21,10 @@ val appModule = module {
 
     viewModel<VacancyViewModel> {
         VacancyViewModel(get(), get())
+    }
+
+    viewModel<FilterRegionViewModel> {
+        FilterRegionViewModel()
     }
 
     viewModel<FilterCountryViewModel> {
