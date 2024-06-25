05a9bbc9428fe7b4893f0625aee88fe51c47c403
dencharski <dencharski@yandex.ru>
2024-06-25 14:56:30 +0200

    -create interactor and repository write and read filtrationparams in SHARED_PREFERENCES.

--- app/src/main/java/ru/practicum/android/diploma/data/filtration/impl/FiltrationParamsSaveRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/filtration/impl/FiltrationParamsSaveRepositoryImpl.kt
@@ -3,27 +3,58 @@ package ru.practicum.android.diploma.data.filtration.impl
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import ru.practicum.android.diploma.data.filtration.FiltrationParamsSaveRepository
-import ru.practicum.android.diploma.util.FILTRATION_KEY
+import ru.practicum.android.diploma.domain.filtration.models.AreaDomain
+import ru.practicum.android.diploma.domain.filtration.models.FilterRegionArea
+import ru.practicum.android.diploma.domain.filtration.models.IndustryDomain
+import ru.practicum.android.diploma.util.FILTRATION_AREA_KEY
+import ru.practicum.android.diploma.util.FILTRATION_INDUSTRY_KEY
+import ru.practicum.android.diploma.util.FILTRATION_REGION_KEY
 
 class FiltrationParamsSaveRepositoryImpl(
     private val sharedPreferences: SharedPreferences
 ) : FiltrationParamsSaveRepository {
-    override fun getFilterParams(): List<String> {
-        val json = sharedPreferences.getString(FILTRATION_KEY, null) ?: return emptyList()
-        return Gson().fromJson(json, Array<String>::class.java).toList()
+    override fun getAreaFilterParams(): AreaDomain? {
+        val json = sharedPreferences.getString(FILTRATION_AREA_KEY, null) ?: return null
+        return Gson().fromJson(json, AreaDomain::class.java)
     }
 
-    override fun saveFilterParams(listFilterParams: List<String>) {
-        removeFilterParamsInSharedPreferences()
-        val json = Gson().toJson(listFilterParams)
+    override fun saveAreaFilterParams(areaDomain: AreaDomain) {
+        removeFilterParamsInSharedPreferences(FILTRATION_AREA_KEY)
+        val json = Gson().toJson(areaDomain)
         sharedPreferences.edit()
-            .putString(FILTRATION_KEY, json)
+            .putString(FILTRATION_AREA_KEY, json)
             .apply()
     }
 
-    private fun removeFilterParamsInSharedPreferences() {
+    override fun getRegionFilterParams(): FilterRegionArea? {
+        val json = sharedPreferences.getString(FILTRATION_REGION_KEY, null) ?: return null
+        return Gson().fromJson(json, FilterRegionArea::class.java)
+    }
+
+    override fun saveRegionFilterParams(filterRegionArea: FilterRegionArea) {
+        removeFilterParamsInSharedPreferences(FILTRATION_REGION_KEY)
+        val json = Gson().toJson(filterRegionArea)
         sharedPreferences.edit()
-            .remove(FILTRATION_KEY)
+            .putString(FILTRATION_AREA_KEY, json)
+            .apply()
+    }
+
+    override fun getIndustryFilterParams(): IndustryDomain? {
+        val json = sharedPreferences.getString(FILTRATION_INDUSTRY_KEY, null) ?: return null
+        return Gson().fromJson(json, IndustryDomain::class.java)
+    }
+
+    override fun saveIndustryFilterParams(industryDomain: IndustryDomain) {
+        removeFilterParamsInSharedPreferences(FILTRATION_INDUSTRY_KEY)
+        val json = Gson().toJson(industryDomain)
+        sharedPreferences.edit()
+            .putString(FILTRATION_AREA_KEY, json)
+            .apply()
+    }
+
+    private fun removeFilterParamsInSharedPreferences(key: String) {
+        sharedPreferences.edit()
+            .remove(key)
             .apply()
     }
 }
