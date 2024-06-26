a74da6c5e896a59033f3cc6dc911c9efafea5e6b
Akhmed <shahidow@mail.ru>
2024-06-26 23:05:53 +0200

    correct

--- app/src/main/java/ru/practicum/android/diploma/domain/filtration/models/FilterParams.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filtration/models/FilterParams.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.domain.filtration.models
 
 data class FilterParams(
     val areaDomain: AreaDomain,
-    val filterRegionArea: FilterRegionArea,
+    val filterRegionArea: AreaDomain,
     val industryDomain: IndustryDomain,
     val salary: Int,
     val showOnlyWithSalary: Boolean
