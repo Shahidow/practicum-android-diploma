1c32a54cb3f2e792deb6c226175dfc1dceadc026
dencharski <dencharski@yandex.ru>
2024-06-26 15:21:29 +0200

    -edit interactor and repository write and read filtrationparams in SHARED_PREFERENCES.

--- app/src/main/java/ru/practicum/android/diploma/domain/filtration/impl/FiltrationParamsSaveInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filtration/impl/FiltrationParamsSaveInteractorImpl.kt
@@ -2,34 +2,20 @@ package ru.practicum.android.diploma.domain.filtration.impl
 
 import ru.practicum.android.diploma.data.filtration.FiltrationParamsSaveRepository
 import ru.practicum.android.diploma.domain.filtration.FiltrationParamsSaveInteractor
-import ru.practicum.android.diploma.domain.filtration.models.AreaDomain
-import ru.practicum.android.diploma.domain.filtration.models.FilterRegionArea
-import ru.practicum.android.diploma.domain.filtration.models.IndustryDomain
+import ru.practicum.android.diploma.domain.filtration.models.FilterParams
 
 class FiltrationParamsSaveInteractorImpl(
     private val filtrationParamsSaveRepository: FiltrationParamsSaveRepository
 ) : FiltrationParamsSaveInteractor {
-    override fun getAreaFilterParams(): AreaDomain? {
-        return filtrationParamsSaveRepository.getAreaFilterParams()
+    override fun getFilterParams(): FilterParams? {
+        return filtrationParamsSaveRepository.getFilterParams()
     }
 
-    override fun saveAreaFilterParams(areaDomain: AreaDomain) {
-        filtrationParamsSaveRepository.saveAreaFilterParams(areaDomain)
+    override fun saveFilterParams(filterParams: FilterParams) {
+        filtrationParamsSaveRepository.saveFilterParams(filterParams)
     }
 
-    override fun getRegionFilterParams(): FilterRegionArea? {
-        return filtrationParamsSaveRepository.getRegionFilterParams()
-    }
-
-    override fun saveRegionFilterParams(filterRegionArea: FilterRegionArea) {
-        filtrationParamsSaveRepository.saveRegionFilterParams(filterRegionArea)
-    }
-
-    override fun getIndustryFilterParams(): IndustryDomain? {
-        return filtrationParamsSaveRepository.getIndustryFilterParams()
-    }
-
-    override fun saveIndustryFilterParams(industryDomain: IndustryDomain) {
-        filtrationParamsSaveRepository.saveIndustryFilterParams(industryDomain)
+    override fun removeFilterParams() {
+        filtrationParamsSaveRepository.removeFilterParams()
     }
 }
