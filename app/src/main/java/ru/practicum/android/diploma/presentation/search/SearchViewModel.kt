2800eb8ba8709c5107e33b52e30aa0c8601c62a0
purpleratchet <ibeater01@gmail.com>
2024-07-01 12:34:43 +0300

    Изменил delay

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchViewModel.kt
@@ -98,9 +98,11 @@ class SearchViewModel(
     }
 
     fun onResume() {
-        IsLastPage.IS_LAST_PAGE = true
-        currentPage = 0
-        vacanciesList.clear()
-        searchText?.let { searchVacancy(it) }
+        if (vacanciesList.isEmpty()) {
+            currentPage = 0
+            searchText?.let { searchVacancy(it) }
+        } else {
+            searchState.postValue(SearchState.Success(vacanciesList, searchInteractor.foundItems!!))
+        }
     }
 }
